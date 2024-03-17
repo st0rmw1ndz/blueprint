268f22abab2a3752f729e8758349d068bc72c250
frosty <inthishouseofcards@gmail.com>
2024-03-17 00:46:15 -0400

    cli parser

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/st0rmw1ndz/blueprint
 
 go 1.18
+
+require github.com/sirupsen/logrus v1.9.3
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "blueprint",
+	Short: "Project initialization system",
+	Run: func(cmd *cobra.Command, args []string) {
+		if quietFlag {
+			logrus.SetLevel(logrus.PanicLevel)
+		}
+		logrus.Info("Hello, world!")
+	},
+}
+
+var quietFlag bool
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&quietFlag, "quiet", "q", false, "quiet mode")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
 }
